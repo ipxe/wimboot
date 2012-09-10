7b9c73e934c4813f39d65032d57ededef1b8be13
Michael Brown <mbrown@fensystems.co.uk>
2012-09-10 14:36:43 +0100

    [build] Allow building on older gcc versions
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- stdint.h
+++ stdint.h
@@ -27,19 +27,19 @@
  *
  */
 
-typedef __UINT8_TYPE__ uint8_t;
-typedef __UINT16_TYPE__ uint16_t;
-typedef __UINT32_TYPE__ uint32_t;
-typedef __UINT64_TYPE__ uint64_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
 
-typedef __INT8_TYPE__ int8_t;
-typedef __INT16_TYPE__ int16_t;
-typedef __INT32_TYPE__ int32_t;
-typedef __INT64_TYPE__ int64_t;
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef signed long long int64_t;
+
+typedef unsigned long intptr_t;
 
 typedef __SIZE_TYPE__ size_t;
-
-typedef __INTPTR_TYPE__ intptr_t;
 
 typedef __WCHAR_TYPE__ wchar_t;
 typedef __WINT_TYPE__ wint_t;
