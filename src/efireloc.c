91be50c17d4d9f463109d5baafd70f9fdadd86db
Alif M. Ahmad <alive4ever@live.com>
2018-12-17 03:18:35 +0000

    [build] Add R_X86_64_PLT32 as known reloc target
    According to a discussion on iPXE mailing list[1], binutils 2.31.0
    generates R_X86_64_PLT32 instead of R_X86_64_PC32.
    
    This change fixes building wimboot on systems with binutils >= 2.31.0
    
    [1] http://lists.ipxe.org/pipermail/ipxe-devel/2018-August/006255.html
    
    Signed-off-by: Alif M. Ahmad <alive4ever@live.com>
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- src/efireloc.c
+++ src/efireloc.c
@@ -326,7 +326,8 @@ static void process_reloc ( bfd *bfd __unused, asection *section, arelent *rel,
 		/* Generate a 2-byte PE relocation */
 		generate_pe_reloc ( pe_reltab, offset, 2 );
 	} else if ( ( strcmp ( howto->name, "R_386_PC32" ) == 0 ) ||
-		    ( strcmp ( howto->name, "R_X86_64_PC32" ) == 0 ) ) {
+		    ( strcmp ( howto->name, "R_X86_64_PC32" ) == 0 ) ||
+		    ( strcmp ( howto->name, "R_X86_64_PLT32" ) == 0 ) ) {
 		/* Skip PC-relative relocations; all relative offsets
 		 * remain unaltered when the object is loaded.
 		 */
