8199f22f764693847a1c7088fb4d50f0bfc8a7d6
Michael Brown <mbrown@fensystems.co.uk>
2023-11-26 22:28:08 +0000

    [efi] Do not rely on ProcessorBind.h when building host binaries
    We cannot rely on the EDK2 ProcessorBind.h headers when compiling a
    binary for execution on the build host itself (e.g. efireloc), since
    the host's CPU architecture may not even be supported by EDK2.
    
    Fix by skipping ProcessorBind.h when building a host binary, and
    defining the bare minimum required to allow other EDK2 headers to
    compile cleanly.
    
    This mirrors iPXE commit a99e435 ("[efi] Do not rely on
    ProcessorBind.h when building host binaries").
    
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- src/efi/ProcessorBind.h
+++ src/efi/ProcessorBind.h
@@ -8,12 +8,49 @@
  *
  */
 
-#if __i386__
+#ifdef EFI_HOSTONLY
+
+/*
+ * We cannot rely on the EDK2 ProcessorBind.h headers when compiling a
+ * binary for execution on the build host itself, since the host's CPU
+ * architecture may not even be supported by EDK2.
+ */
+
+/* Define the basic integer types in terms of the host's <stdint.h> */
+#include <stdint.h>
+typedef int8_t INT8;
+typedef int16_t INT16;
+typedef int32_t INT32;
+typedef int64_t INT64;
+typedef uint8_t UINT8;
+typedef long INTN;
+typedef uint16_t UINT16;
+typedef uint32_t UINT32;
+typedef uint64_t UINT64;
+typedef unsigned long UINTN;
+typedef int8_t CHAR8;
+typedef int16_t CHAR16;
+typedef uint8_t BOOLEAN;
+
+/* Define EFIAPI as whatever API the host uses by default */
+#define EFIAPI
+
+/* Define an architecture-neutral MDE_CPU macro to prevent build errors */
+#define MDE_CPU_EBC
+
+/* Define MAX_BIT in terms of UINTN */
+#define MAX_BIT ( ( ( UINTN ) 1U ) << ( ( 8 * sizeof ( UINTN ) ) - 1 ) )
+
+#else /* EFI_HOSTONLY */
+
+#ifdef __i386__
 #include <efi/Ia32/ProcessorBind.h>
 #endif
 
-#if __x86_64__
+#ifdef __x86_64__
 #include <efi/X64/ProcessorBind.h>
 #endif
+
+#endif /* EFI_HOSTONLY */
 
 #endif /* _WIMBOOT_EFI_PROCESSOR_BIND_H */
